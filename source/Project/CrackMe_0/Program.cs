ad2f695dc38b6b24bfb9f8e249ebc774d3b1c6ef
sunnamed <65300126+sunnamed434@users.noreply.github.com>
2022-03-22 19:49:39 +0200

    Initial commit

---
+++ source/Project/CrackMe_0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CrackMe_0
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            if (password == "hobbits")
+            {
+                Console.WriteLine("Password is right! Good job!");
+            }
+            else
+            {
+                Console.WriteLine("Password is not right! Bad job..");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
